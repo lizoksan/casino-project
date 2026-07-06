5805dd29bdc5742fc88f3e3ea37c6da149ae123d
Liza <lizoksan@gmail.com>
2026-07-06 20:48:19 +0300

    Add Swiper slider to quests, new drawing section, tournament styles, button hover fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,22 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 const SELECTORS = {};
 
 const CLASSNAMES = {};
 
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+	const swiper = new Swiper('.quests__swiper', {
+		modules: [Navigation],
+
+		spaceBetween: 89,
+		loop: true,
+
+		navigation: {
+			nextEl: '.quests__button--next',
+			prevEl: '.quests__button--prev',
+		},
+	});
+});
